175e227df463a1a8866b06580ec235a48f015843
zhou_cong <cong.zhou@1hai.cn>
2021-01-27 14:18:52 +0800

    🔥 Delete: 删除登录页

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,63 +1,16 @@
 import React from 'react';
-import type { Settings as LayoutSettings } from '@ant-design/pro-layout';
-import { PageLoading } from '@ant-design/pro-layout';
 import { notification } from 'antd';
 import type { RequestConfig, RunTimeLayoutConfig } from 'umi';
-import { history } from 'umi';
 import RightContent from '@/components/RightContent';
 import Footer from '@/components/Footer';
 import type { ResponseError } from 'umi-request';
-import { queryCurrent } from './services/user';
-import defaultSettings from '../config/defaultSettings';
-
-/**
- * 获取用户信息比较慢的时候会展示一个 loading
- */
-export const initialStateConfig = {
-  loading: <PageLoading />,
-};
-
-export async function getInitialState(): Promise<{
-  settings?: LayoutSettings;
-  currentUser?: API.CurrentUser;
-  fetchUserInfo?: () => Promise<API.CurrentUser | undefined>;
-}> {
-  const fetchUserInfo = async () => {
-    try {
-      const currentUser = await queryCurrent();
-      return currentUser;
-    } catch (error) {
-      history.push('/user/login');
-    }
-    return undefined;
-  };
-  // 如果是登录页面，不执行
-  if (history.location.pathname !== '/user/login') {
-    const currentUser = await fetchUserInfo();
-    return {
-      fetchUserInfo,
-      currentUser,
-      settings: defaultSettings,
-    };
-  }
-  return {
-    fetchUserInfo,
-    settings: defaultSettings,
-  };
-}
 
 export const layout: RunTimeLayoutConfig = ({ initialState }) => {
   return {
     rightContentRender: () => <RightContent />,
     disableContentMargin: false,
     footerRender: () => <Footer />,
-    onPageChange: () => {
-      const { location } = history;
-      // 如果没有登录，重定向到 login
-      if (!initialState?.currentUser && location.pathname !== '/user/login') {
-        history.push('/user/login');
-      }
-    },
+    onPageChange: () => {},
     menuHeaderRender: undefined,
     // 自定义 403 页面
     // unAccessible: <div>unAccessible</div>,
